8d213f717e22deb8ac6d7803aeb385e29052be44
alter <alter.pub@gmail.com>
2013-01-24 21:32:42 +0400

    changes for ruby 1.9.3

--- lib/bashim.rb
+++ lib/bashim.rb
@@ -1,19 +1,20 @@
+# encoding: utf-8
 require "bashim/version"
 require 'net/http'
 require 'iconv'
 
 module Bashim
-
-	html = Net::HTTP.get('bash.im', '/random')
-	html = Iconv.conv("UTF8//IGNORE//TRANSLIT", "CP1251", html)
-	text = /\<div class=\"text\"\>.*?\<\/div\>/.match(html)
-	cit = text.to_s
-	cit.gsub!(/\<br.*?\>/,"\n")
-	cit.gsub!(/&quot;/,"\"")
-	cit.gsub!(/&lt;/,"<")
-	cit.gsub!(/&gt;/,">")
-	puts "\033[33m"
-	puts cit.gsub(/\<.+?\>/,"")
-	puts "\033[37m"
-
+	def random
+		html = Net::HTTP.get('bash.im', '/random')
+		html = html.encode Encoding::UTF_8, :undef => :replace
+		text = /\<div class=\"text\"\>.*?\<\/div\>/.match(html)
+		cit = text.to_s
+		cit.gsub!(/\<br.*?\>/,"\n")
+		cit.gsub!(/&quot;/,"\"")
+		cit.gsub!(/&lt;/,"<")
+		cit.gsub!(/&gt;/,">")
+		puts "\033[33m"
+		puts cit.gsub(/\<.+?\>/,"")
+		puts "\033[37m"
+	end
 end
